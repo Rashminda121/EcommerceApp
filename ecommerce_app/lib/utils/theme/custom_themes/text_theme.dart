218cac913bf9c3ce98e187497be5e9e725d4f955
Rashminda <rashminda121@gmail.com>
2024-03-03 23:00:46 +0530

    update files

---
+++ ecommerce_app/lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme{
+  TTextTheme._();
+
+
+  static TextTheme lightTextTheme = TextTheme(
+    
+  );
+  static TextTheme darkTextTheme = TextTheme();
+
+}
