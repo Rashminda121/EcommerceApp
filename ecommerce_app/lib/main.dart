3757b33a66f97f4acb4944c4922d3501796f9747
Rashminda <rashminda121@gmail.com>
2024-03-05 09:07:59 +0530

    main dart update

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,20 +1,13 @@
+import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/utils/theme/theme.dart';
 
+import 'app.dart';
+
 void main() {
-  runApp(App());
+
+
+  runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      key: GlobalKey(), // Provide a GlobalKey here
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
