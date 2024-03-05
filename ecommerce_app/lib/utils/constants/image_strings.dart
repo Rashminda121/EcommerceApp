086ae55a96b073ed2be59f96f009046af0c74141
Rashminda <rashminda121@gmail.com>
2024-03-05 09:08:58 +0530

    added image file strings

---
+++ ecommerce_app/lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // -- App Logos
+  static const String darkAppLogo = "assets/logos/darklogo.png";
+  static const String lightAppLogo = "assets/logos/lightlogo.png";
+
+  // -- Social Logos
+  static const String google = "assets/logos/facebook.png";
+  static const String facebook = "assets/logos/google.png";
+}
