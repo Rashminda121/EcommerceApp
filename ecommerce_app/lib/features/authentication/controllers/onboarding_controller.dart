20b30615ecd959a887a433371bf9f2614409b3e0
Rashminda <rashminda121@gmail.com>
2024-03-05 21:32:19 +0530

    onboarding controller update

--- ecommerce_app/lib/features/authentication/controllers/onboarding_controller.dart
+++ ecommerce_app/lib/features/authentication/controllers/onboarding_controller.dart
@@ -1,19 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OnBoardingController extends GetxController {
   static OnBoardingController get instance => Get.find();
 
   /// Variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex= 0.obs;
+
 
   /// Update Current Index when Page Scroll
-  void updatePageIndicator(int index) {}
+  void updatePageIndicator(index) => currentPageIndex.value = index;
 
   /// Jump to the specific dot selected page.
-  void dotNavigationClick(int index) {}
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
 
   /// Update Current Index & jump to next page
-  void nextPage() {}
+  void nextPage() {
+    if(currentPageIndex.value == 2){
+      //Get.to(LoginScreen()); /// screen need to take to after clicking next button
+    }else{
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
 
   /// Update Current Index & jump to the last Page
-  void skipPage() {}
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
 }
