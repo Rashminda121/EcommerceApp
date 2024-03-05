611cf77985dc7557387bc6ea011ef0a3bb8ad10f
Rashminda <rashminda121@gmail.com>
2024-03-05 09:07:45 +0530

    add dart update

---
+++ ecommerce_app/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+    );
+  }
+}
