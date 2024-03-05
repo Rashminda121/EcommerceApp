4ffd6654deece1d3a3ace737db3038fc152b477c
Rashminda <rashminda121@gmail.com>
2024-03-05 21:33:04 +0530

    dot navigation update

--- ecommerce_app/lib/features/authentication/screens/onboarding/widgets/onboardingDotNavigation.dart
+++ ecommerce_app/lib/features/authentication/screens/onboarding/widgets/onboardingDotNavigation.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/features/authentication/controllers/onboarding_controller.dart';
 import 'package:ecommerce_app/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -14,14 +15,17 @@ class OnboardingDotNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final controller = OnBoardingController.instance;
     final dark=THelperFunctions.isDarkMode(context);
 
     return Positioned(
         bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
         left: TSizes.defaultSpace,
         child: SmoothPageIndicator(
-          controller: PageController(),
           count: 3,
+          controller: controller.pageController,
+          onDotClicked: controller.dotNavigationClick,
+
           effect: ExpandingDotsEffect(
             activeDotColor: dark ? TColors.light : TColors.dark , //dot navigation colour
             dotHeight: 6,
