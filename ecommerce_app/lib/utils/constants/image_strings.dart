2ca0c0e3f289b6388dec88e5852de0bcb467a98d
Rashminda <rashminda121@gmail.com>
2024-03-06 22:42:39 +0530

    update text and image strings

--- ecommerce_app/lib/utils/constants/image_strings.dart
+++ ecommerce_app/lib/utils/constants/image_strings.dart
@@ -4,8 +4,8 @@ class TImages {
   static const String lightAppLogo = "assets/logos/lightlogo.png";
 
   // -- Social Logos
-  static const String google = "assets/logos/facebook.png";
-  static const String facebook = "assets/logos/google.png";
+  static const String google = "assets/logos/google.png";
+  static const String facebook = "assets/logos/facebook.png";
 
   //onboarding
   static const String onboard1 = "assets/images/on_boarding_images/w1.gif";
--- ecommerce_app/lib/utils/constants/text_strings.dart
+++ ecommerce_app/lib/utils/constants/text_strings.dart
@@ -10,4 +10,39 @@ class TTexts {
   // -- Home
   static const String homeAppbarTitle = "Good day for shopping";
   static const String homeAppbarSubTitle = "Taimoor Sikander";
+
+  //Authentication Form Text
+  static const String firstName= "First Name";
+  static const String LastName= "Last Nane";
+  static const String email= "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo= "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn= "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith="or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo= "I agree to";
+  static const String privacyPolicy="Privacy Policy";
+  static const String terasofUse = "Terms of use";
+  static const String verificationCode="verificationCode";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+
+
+
+  // -- Authentication Headings Text
+  static const String loginTitle = "Welcome back,";
+  static const String LoginSubTitle = "Discover Limitless Choices and Unmatched Convenience.";
+  static const String signupTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forget password";
+  static const String forgetPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password reset link.";
+  static const String changePasswordTitle = "Password Reset Email Sent";
+  static const String changePasswordSubTitle = "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle = "Congratulations! Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
+  static const String emailNotReceivedMessage = "Didn't get the email? Check your junk/spam or resend it.";
 }
