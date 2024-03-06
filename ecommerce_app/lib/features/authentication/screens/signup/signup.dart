4fde7473976650a37a55919ddeeebf5a5a76b794
Rashminda <rashminda121@gmail.com>
2024-03-06 22:42:23 +0530

    added signup dart

---
+++ ecommerce_app/lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+        ),
+      ),
+    );
+  }
+}
+
