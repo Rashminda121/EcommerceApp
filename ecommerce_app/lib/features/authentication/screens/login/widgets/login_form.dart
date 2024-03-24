ec8ed0a19ab2dfafb030dcebb31b38e5f512e02d
Rashminda <rashminda121@gmail.com>
2024-03-24 11:29:11 +0530

    Update login_form.dart

--- ecommerce_app/lib/features/authentication/screens/login/widgets/login_form.dart
+++ ecommerce_app/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:ecommerce_app/features/authentication/screens/signup/signup.dart';
+import 'package:ecommerce_app/navigation_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -67,7 +68,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: ()=>Get.to(()=>const NavigationMenu()),
                 child: const Text(TTexts.signIn),
               ),
             ),
