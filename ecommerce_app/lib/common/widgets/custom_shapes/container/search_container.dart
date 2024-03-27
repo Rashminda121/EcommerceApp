8ebc62ea55c2fdcaf5cb9fe42e70fe2f70b4d470
Rashminda <rashminda121@gmail.com>
2024-03-27 11:23:31 +0530

    Update search_container.dart

--- ecommerce_app/lib/common/widgets/custom_shapes/container/search_container.dart
+++ ecommerce_app/lib/common/widgets/custom_shapes/container/search_container.dart
@@ -8,38 +8,48 @@ import '../../../../utils/helpers/helper_functions.dart';
 
 class TSearchContainer extends StatelessWidget {
   const TSearchContainer({
-    super.key, required this.text, this.icon=Iconsax.search_normal, this.showBackground=true, this.showBorder=true,
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+    this.onTap,
   });
 
   final String text;
-  final IconData?icon;
-  final bool showBackground,showBorder;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
 
-    final dark =THelperFunctions.isDarkMode(context);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context),
-        padding: const EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-            color:  showBackground ? dark ? TColors.dark : TColors.light : Colors.transparent,
-            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-            border: showBorder ? Border.all(color: TColors.grey): null
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+              color: showBackground
+                  ? dark
+                      ? TColors.dark
+                      : TColors.light
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+              border: showBorder ? Border.all(color: TColors.grey) : null),
+          child: Row(
+            children: [
+              Icon(icon, color: dark ? TColors.light : TColors.darkerGrey),
+              const SizedBox(width: TSizes.spaceBtwItems),
+              Text(text,
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: dark ? TColors.grey : TColors.darkerGrey)),
+            ],
+          ),
         ),
-
-        child: Row(
-          children: [
-            Icon(icon,color:  TColors.darkerGrey ),
-            const SizedBox(width: TSizes.spaceBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall?.copyWith(color: TColors.darkGrey)),
-
-          ],
-        ),
-
       ),
     );
   }
