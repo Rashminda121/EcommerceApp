bfb17aef95b0599e846bccfd1c71d64e18d6cf3a
Rashminda <rashminda121@gmail.com>
2024-03-27 17:39:11 +0530

    Update product_card_vertical.dart

--- ecommerce_app/lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ ecommerce_app/lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,24 +1,93 @@
-import 'package:ecommerce_app/common/widgets/products/product_cards/widgets/TShadowStyle.dart';
+import 'package:ecommerce_app/common/styles/shadows.dart';
+import 'package:ecommerce_app/common/widgets/images/t_rounded_image.dart';
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/helpers/helper_functions.dart';
+import '../../../styles/rounded_container.dart';
+import '../../icons/t_circular_icon.dart';
 
 class TProductCardVertical extends StatelessWidget {
   const TProductCardVertical({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    ///container with side padding
     return Container(
       width: 180,
       padding: const EdgeInsets.all(1),
       decoration: BoxDecoration(
         boxShadow: [TShadowStyle.verticalProductShadow],
         borderRadius: BorderRadius.circular(TSizes.productImageRadius),
-        color: THelperFunctions.isDarkMode(context)
-            ? TColors.darkerGrey
-            : TColors.white,
+        color: dark ? TColors.darkerGrey : TColors.white,
+      ),
+      child: Column(
+        children: [
+          /// Thumbnail, Wishlist Button, Discount Tag
+          TRoundedContainer(
+            height: 180,
+            padding: const EdgeInsets.all(TSizes.sm),
+            backgroundColor: dark ? TColors.dark : TColors.light,
+            child: Stack(
+              children: [
+                ///Thumbnail Image
+                const TRoundedImage(
+                    imageUrl: TImages.product1, applyImageRadius: true),
+
+                ///sale tag
+                Positioned(
+                  top: 12,
+                  child: TRoundedContainer(
+                    radius: TSizes.sm,
+                    backgroundColor: TColors.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: TSizes.sm, vertical: TSizes.xs),
+                    child: Text(
+                      '25 %',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .apply(color: TColors.black),
+                    ),
+                  ),
+                ),
+
+                ///fovaourite icon button
+                const Positioned(
+                  top: 0,
+                  right: 0,
+                  child: TCircularIcon(
+                    icon: Iconsax.heart5,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          ///detail
+          Padding(
+            padding: const EdgeInsets.only(left: TSizes.sm),
+            child: Column(
+              children: [
+                Text(
+                  'Green Nike Air Shoes',
+                  style: Theme.of(context).textTheme.labelLarge,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
