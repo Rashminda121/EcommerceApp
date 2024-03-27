ec215371b307d7a6c984a12a20346daa5968473e
Rashminda <rashminda121@gmail.com>
2024-03-27 09:40:47 +0530

    Update colors.dart

--- ecommerce_app/lib/utils/constants/colors.dart
+++ ecommerce_app/lib/utils/constants/colors.dart
@@ -20,7 +20,7 @@ class TColors{
 
   //Background Colors
   static const Color light = Color(0xFFF6F6F6);
-  static const Color dark = Color(0xFF2727227);
+  static const Color dark = Color(0xdd272722);
   static const Color primaryBackground= Color(0xFFF3F5FF);
 
   //Background Container Colors
