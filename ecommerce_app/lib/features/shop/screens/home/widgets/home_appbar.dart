65fea8af5cf3c96e0b10d57a25c0cf45b207da48
Rashminda <rashminda121@gmail.com>
2024-03-27 08:44:28 +0530

    Update home_appbar.dart

--- ecommerce_app/lib/features/shop/screens/home/widgets/home_appbar.dart
+++ ecommerce_app/lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -1,30 +1,30 @@
-// import 'package:flutter/material.dart';
-//
-// import '../../../../../common/widgets/appbar/appbar.dart';
-// import '../../../../../common/widgets/products/cart/cart_menu_icon.dart';
-// import '../../../../../utils/constants/colors.dart';
-// import '../../../../../utils/constants/text_strings.dart';
-//
-// class THomeAppBar extends StatelessWidget {
-//   const THomeAppBar({
-//     super.key,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return TAppBar(
-//       title: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
-//           Text(TTexts.homeAppbarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
-//         ],
-//       ),
-//       actions: [
-//         TCartCountericon(onPressed: () {  }, iconColor: TColors.white,)
-//       ],
-//
-//
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class THomeAppBar extends StatelessWidget {
+  const THomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
+          Text(TTexts.homeAppbarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
+        ],
+      ),
+      actions: [
+        TCartCountericon(onPressed: () {  }, iconColor: TColors.white,)
+      ],
+
+
+    );
+  }
+}
