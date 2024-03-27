1a56b3675e15030746df4ebc15cd53c2c0f24743
Rashminda <rashminda121@gmail.com>
2024-03-27 09:40:45 +0530

    Update home.dart

--- ecommerce_app/lib/features/shop/screens/home/home.dart
+++ ecommerce_app/lib/features/shop/screens/home/home.dart
@@ -1,13 +1,14 @@
 
 
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/custom_shapes/container/search_container.dart';
 import '../../../../utils/constants/colors.dart';
 import '../../../../utils/constants/sizes.dart';
-import '../../../../utils/constants/text_strings.dart';
 import '../../../../utils/device/device_utility.dart';
 
 
@@ -25,22 +26,17 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   ///App Bar
                   THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
                   /// search bar
 
-                  Container(
-                    width: TDeviceUtils.getScreenWidth(context),
-                    padding: const EdgeInsets.all(TSizes.md),
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-                      border: Border.all(color: TColors.grey),
-                    ),
-                  ),
+                  TSearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
 
                   /// categories
 
+                  
 
 
                 ],
@@ -52,6 +48,8 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
 
