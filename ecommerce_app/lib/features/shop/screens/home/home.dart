22212fa7fdc2a02078ce25a8d977241e7e6c8551
Rashminda <rashminda121@gmail.com>
2024-03-27 08:56:15 +0530

    Update home.dart

--- ecommerce_app/lib/features/shop/screens/home/home.dart
+++ ecommerce_app/lib/features/shop/screens/home/home.dart
@@ -6,7 +6,9 @@ import 'package:flutter/material.dart';
 import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/device/device_utility.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -14,7 +16,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -25,6 +27,17 @@ class HomeScreen extends StatelessWidget {
                   THomeAppBar(),
 
                   /// search bar
+
+                  Container(
+                    width: TDeviceUtils.getScreenWidth(context),
+                    padding: const EdgeInsets.all(TSizes.md),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+                      border: Border.all(color: TColors.grey),
+                    ),
+                  ),
+
 
                   /// categories
 
