785be1cc6979252824ea2c244ccd393d3532350b
Rashminda <rashminda121@gmail.com>
2024-04-04 22:01:47 +0530

    Update login.dart

--- ecommerce_app/lib/features/authentication/screens/login/login.dart
+++ ecommerce_app/lib/features/authentication/screens/login/login.dart
@@ -1,14 +1,11 @@
 import "package:ecommerce_app/common/styles/spacing_styles.dart";
 import 'package:ecommerce_app/common/widgets/login_signup/login_form_divider.dart';
 import "package:ecommerce_app/features/authentication/screens/login/widgets/login_header.dart";
-import "package:ecommerce_app/utils/constants/colors.dart";
-import "package:ecommerce_app/utils/constants/image_strings.dart";
 import "package:ecommerce_app/utils/constants/sizes.dart";
 import "package:ecommerce_app/utils/constants/text_strings.dart";
 import "package:ecommerce_app/utils/helpers/helper_functions.dart";
 import "package:flutter/material.dart";
 import "package:get/get.dart";
-import "package:iconsax/iconsax.dart";
 
 import "widgets/login_form.dart";
 import '../../../../common/widgets/login_signup/social_buttons.dart';
