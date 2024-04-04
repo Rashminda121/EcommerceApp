59ac18ef0aa08e6d55e6b9c349c989dc3681a498
Rashminda <rashminda121@gmail.com>
2024-04-04 10:28:05 +0530

    Update colors.dart

--- ecommerce_app/lib/utils/constants/colors.dart
+++ ecommerce_app/lib/utils/constants/colors.dart
@@ -4,7 +4,7 @@ class TColors{
   TColors._();
 
   //App Basic Color
-  static const Color primaryColor = Color(0xFF4B68FF);
+  static const Color primary = Color(0xFF4B68FF);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
