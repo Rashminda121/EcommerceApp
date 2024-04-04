8bd5c686058b40644a2194a8b6be9182a42ab3df
Rashminda <rashminda121@gmail.com>
2024-04-04 22:01:35 +0530

    Update t_circular_icon.dart

--- ecommerce_app/lib/common/widgets/icons/t_circular_icon.dart
+++ ecommerce_app/lib/common/widgets/icons/t_circular_icon.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 import '../../../utils/constants/colors.dart';
 
