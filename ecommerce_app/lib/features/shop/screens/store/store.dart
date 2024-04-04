48cfb38f9dba5652ba70c7e2f51f03d10352e48a
Rashminda <rashminda121@gmail.com>
2024-04-04 22:02:05 +0530

    Update store.dart

--- ecommerce_app/lib/features/shop/screens/store/store.dart
+++ ecommerce_app/lib/features/shop/screens/store/store.dart
@@ -1,11 +1,10 @@
 import 'package:ecommerce_app/common/widgets/appbar/tabbar.dart';
 import 'package:ecommerce_app/common/widgets/custom_shapes/container/search_container.dart';
 import 'package:ecommerce_app/common/widgets/layouts/grid_layout.dart';
-import 'package:ecommerce_app/features/shop/screens/store/widgets/brand_cart.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:ecommerce_app/features/shop/screens/store/widgets/category_tab.dart';
 import 'package:flutter/material.dart';
-import '../../../../common/styles/rounded_container.dart';
 import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../common/widgets/brands/brand_card.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/colors.dart';
@@ -21,15 +20,16 @@ class StoreScreen extends StatelessWidget {
       length: 5,
       child: Scaffold(
         appBar: TAppBar(
-          title: Text('Store', style: Theme.of(context).textTheme.headlineMedium),
+          title:
+              Text('Store', style: Theme.of(context).textTheme.headlineMedium),
           actions: [
             TCartCountericon(onPressed: () {}),
           ],
         ),
         body: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverAppBar(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
                   automaticallyImplyLeading: false,
                   pinned: true,
                   floating: true,
@@ -38,66 +38,58 @@ class StoreScreen extends StatelessWidget {
                       : TColors.white,
                   expandedHeight: 440,
                   flexibleSpace: Padding(
-                    padding: const EdgeInsets.all(TSizes.defaultSpace),
-                    child:ListView(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: [
-                        ///Search Bar
-                        const SizedBox(height: TSizes.spaceBtwItems),
-                        const TSearchContainer(text: 'Search in Store',showBorder: true,showBackground: false,padding: EdgeInsets.zero),
-                        const SizedBox(height: TSizes.spaceBtwSections),
-      
-                        ///featured brands
-                        TSectionHeading(title: 'Featured Brands', onPressed:(){}),
-                        const SizedBox(height: TSizes.spaceBtwItems/1.5),
-      
-                        TGridLayout(itemCount: 4, mainAxisExtent: 80,itemBuilder: (_,index) {
-                          return const TBrandCard(showBorder: false);
-                        }),
-      
-                      ],
-                    )
-      
-                  ),
-      
+                      padding: const EdgeInsets.all(TSizes.defaultSpace),
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          ///Search Bar
+                          const SizedBox(height: TSizes.spaceBtwItems),
+                          const TSearchContainer(
+                              text: 'Search in Store',
+                              showBorder: true,
+                              showBackground: false,
+                              padding: EdgeInsets.zero),
+                          const SizedBox(height: TSizes.spaceBtwSections),
+
+                          ///featured brands
+                          TSectionHeading(
+                              title: 'Featured Brands', onPressed: () {}),
+                          const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                          TGridLayout(
+                              itemCount: 4,
+                              mainAxisExtent: 80,
+                              itemBuilder: (_, index) {
+                                return const TBrandCard(showBorder: false);
+                              }),
+                        ],
+                      )),
+
                   ///tabs----
                   bottom: const TTabBar(
                     tabs: [
-
                       Tab(child: Text('Sports')),
                       Tab(child: Text('Furniture')),
                       Tab(child: Text('Electronics')),
                       Tab(child: Text('Clothes')),
                       Tab(child: Text('Cosmetics')),
-
-                  ],)
-
-                ),
-              ];
-            },
+                    ],
+                  )),
+            ];
+          },
           body: const TabBarView(
             children: [
-              Padding(
-                padding: EdgeInsets.all(TSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    // Brands
-                    TRoundedContainer(
-                      showBorder: true,
-                      borderColor: TColors.darkGrey,
-                      backgroundColor: Colors.transparent,
-                      margin: EdgeInsets.only(bottom: TSizes.spaceBtwItems),
-                      child: Column(
-                        children: [
-                          TBrandCard(showBorder: false)
-                        ],
+            TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab()
 
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+
+
+
+
             ],
           ),
         ),
@@ -105,5 +97,6 @@ class StoreScreen extends StatelessWidget {
     );
   }
 }
+
 
 
