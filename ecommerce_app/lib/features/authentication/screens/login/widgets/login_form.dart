091ab4c018988e068001b3b8643ae45d8b90cbca
Rashminda <rashminda121@gmail.com>
2024-04-04 22:01:49 +0530

    Update login_form.dart

--- ecommerce_app/lib/features/authentication/screens/login/widgets/login_form.dart
+++ ecommerce_app/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -3,7 +3,6 @@ import 'package:ecommerce_app/features/authentication/screens/signup/signup.dart
 import 'package:ecommerce_app/navigation_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../utils/constants/sizes.dart';
