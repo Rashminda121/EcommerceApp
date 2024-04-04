9e96ef3156d25b469c21909841d659e17332d2dd
Rashminda <rashminda121@gmail.com>
2024-04-04 22:01:51 +0530

    Update onboarding.dart

--- ecommerce_app/lib/features/authentication/screens/onboarding/onboarding.dart
+++ ecommerce_app/lib/features/authentication/screens/onboarding/onboarding.dart
@@ -2,16 +2,10 @@ import "package:ecommerce_app/features/authentication/controllers/onboarding_con
 import "package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboardingDotNavigation.dart";
 import "package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboardingNextButton.dart";
 import "package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_skip.dart";
-import "package:ecommerce_app/utils/constants/colors.dart";
 import "package:ecommerce_app/utils/constants/image_strings.dart";
-import "package:ecommerce_app/utils/constants/sizes.dart";
 import "package:ecommerce_app/utils/constants/text_strings.dart";
-import "package:ecommerce_app/utils/device/device_utility.dart";
-import "package:ecommerce_app/utils/helpers/helper_functions.dart";
 import "package:flutter/material.dart";
 import "package:get/get.dart";
-import "package:iconsax/iconsax.dart";
-import "package:smooth_page_indicator/smooth_page_indicator.dart";
 
 import "widgets/onboarding_page.dart";
 
