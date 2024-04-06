7f5796b34cca8670cedfd3c346b588a6817449c4
Rashminda <rashminda121@gmail.com>
2024-04-06 12:15:03 +0530

    Create product_attributes.dart

---
+++ ecommerce_app/lib/features/shop/screens/product_details/widgets/product_attributes.dart
@@ -0,0 +1,95 @@
+import 'package:ecommerce_app/common/styles/rounded_container.dart';
+import 'package:ecommerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:ecommerce_app/common/widgets/texts/product_title_text.dart';
+import 'package:ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/chips/choice_chip.dart';
+
+class TProductAttributes extends StatelessWidget {
+  const TProductAttributes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark=THelperFunctions.isDarkMode(context);
+    return Column(
+      children: [
+        TRoundedContainer(
+          padding: const EdgeInsets.all(TSizes.md),
+          backgroundColor: dark? TColors.darkerGrey:TColors.grey,
+          child:Column(
+            children: [
+              ///title,price and stock status
+              Row(
+                children: [
+                  TSectionHeading(title: 'Variation',showActionButton: false),
+                  SizedBox(width: TSizes.spaceBtwItems),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      Row(
+                        children: [
+                          TProductTitleText(title: 'Price: ',smallSize: true),
+
+                          /// Actual Price
+                          Text(
+                            '\$25',
+                            style: Theme.of(context).textTheme.titleSmall!.apply(
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                          SizedBox(width: TSizes.spaceBtwItems),
+
+                          ///sale price
+                          TProductPriceText(price: '20'),
+                        ],
+                      ),
+
+                      ///stack
+                      Row(
+                        children: [
+                          const TProductTitleText(title: 'Stock: ', smallSize: true),
+                          Text('In Stock', style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+
+
+                    ],
+                  ),
+
+                ],
+              ),
+
+              /// Variation Description
+
+              const TProductTitleText(
+                title: 'This is the Description of the Product and it can go upto max 4 lines.',
+                smallSize: true,
+                maxLines: 4,
+              ),
+
+            ],
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+
+        ///atributes
+        const Column(
+          children: [
+            TSectionHeading(title: 'Colors'),
+            SizedBox(height: TSizes.spaceBtwItems / 2),
+            TChoiceChip(text: 'Green',selected: true),
+          ],
+        ),
+
+      ],
+    );
+  }
+}
+
+
