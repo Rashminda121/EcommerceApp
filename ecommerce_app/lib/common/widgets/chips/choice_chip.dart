227097d577cad01531c9d3e107f232fce2cc04fe
Rashminda <rashminda121@gmail.com>
2024-04-06 19:02:09 +0530

    Update choice_chip.dart

--- ecommerce_app/lib/common/widgets/chips/choice_chip.dart
+++ ecommerce_app/lib/common/widgets/chips/choice_chip.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 import '../../../utils/constants/colors.dart';
@@ -17,16 +18,25 @@ class TChoiceChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isColor=THelperFunctions.getColor(text) !=null;
     return ChoiceChip(
-      label: Text(text),
+      label: isColor ? const SizedBox() : Text(text),
       selected: selected,
       onSelected: onSelected,
       labelStyle: TextStyle(color: selected ? TColors.white : null),
-      avatar: const TCircularContainer(
-        width: 50,
-        height: 50,
-        backgroundColor: Colors.green,
-      ),
+      avatar: isColor? TCircularContainer(width: 50, height: 50, backgroundColor: THelperFunctions.getColor(text)!,) : null,
+      labelPadding: isColor
+          ? const EdgeInsets.all(0)
+          : null, // Make icon in the center
+      padding: isColor
+          ? const EdgeInsets.all(0)
+          : null,
+      shape: isColor
+          ? const CircleBorder()
+          : null,
+      backgroundColor: isColor
+          ? THelperFunctions.getColor(text)!
+          : null,
     );
   }
 }
