527b364771879bb78ad940f538a315be6e26fef2
Rashminda <rashminda121@gmail.com>
2024-04-07 20:22:48 +0530

    Update product_detail.dart

--- ecommerce_app/lib/features/shop/screens/product_details/product_detail.dart
+++ ecommerce_app/lib/features/shop/screens/product_details/product_detail.dart
@@ -2,10 +2,12 @@ import 'package:ecommerce_app/common/widgets/appbar/appbar.dart';
 import 'package:ecommerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:ecommerce_app/common/widgets/images/t_rounded_image.dart';
 import 'package:ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:ecommerce_app/features/shop/screens/product_details/widgets/bottom_add_to_cart.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:ecommerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:ecommerce_app/features/shop/screens/product_reviews/product_reviews.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter/cupertino.dart';
@@ -27,58 +29,77 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-
             /// product image slider
             TProductImageSlider(),
 
             ///product details
             Padding(
-              padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
+              padding: EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
               child: Column(
                 children: [
-                /// Rating & Share
+                  /// Rating & Share
                   TRatingAndShare(),
 
-
-                /// - Price, Title, Stock, & Brand
+                  /// - Price, Title, Stock, & Brand
                   TProductMetaData(),
 
-                /// -- Attributes
+                  /// -- Attributes
                   TProductAttributes(),
                   const SizedBox(height: TSizes.spaceBtwSections),
-                /// -- Checkout Button
-                 SizedBox(
-                     width:double.infinity,
-                     child: ElevatedButton(onPressed:(){}, child:const Text('Checkout'))),
-                  const SizedBox (height: TSizes.spaceBtwItems),
-                  
-                /// Description
 
-                  const TSectionHeading(title: 'Description',showActionButton: false,),
-                  const SizedBox (height: TSizes.spaceBtwItems),
+                  /// -- Checkout Button
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+
+                  /// Description
+
+                  const TSectionHeading(
+                    title: 'Description',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems),
                   const ReadMoreText(
                     'This is a sleeveless vest from Nike, available in a stylish blue color. It is made from high-quality, breathable materials for maximum comfort and performance. Perfect for running, working out, or any other activity where you need freedom of movement and a cool, comfortable fit.',
                     trimLines: 2,
                     trimMode: TrimMode.Line,
                     trimCollapsedText: 'Show More',
                     trimExpandedText: 'Show less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
                   ),
 
-                /// - Reviews
+                  /// - Reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                          title: 'Reviews (199)', showActionButton: false),
+                      IconButton(
+                          icon: const Icon(Iconsax.arrow_right_3, size: 18),
+                          onPressed: () => Get.to(()=>const ProductReviewsScreen())),
+                    ],
+                  ),
                   const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             )
-
           ],
         ),
       ),
     );
   }
 }
-
-
-
