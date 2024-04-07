c552494b20d567a18020b66f16bce5b954f7f170
Rashminda <rashminda121@gmail.com>
2024-04-07 23:31:32 +0530

    Update product_reviews.dart

--- ecommerce_app/lib/features/shop/screens/product_reviews/product_reviews.dart
+++ ecommerce_app/lib/features/shop/screens/product_reviews/product_reviews.dart
@@ -1,18 +1,10 @@
-import 'package:ecommerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
-import 'package:ecommerce_app/features/shop/screens/product_reviews/widgets/progress_indicator_and_rating.dart';
 import 'package:ecommerce_app/features/shop/screens/product_reviews/widgets/rating_progress_indicator.dart';
 import 'package:ecommerce_app/features/shop/screens/product_reviews/widgets/user_review_card.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
-import 'package:ecommerce_app/utils/device/device_utility.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/products/ratings/rating_indicator.dart';
-import '../../../../utils/constants/colors.dart';
 
 class ProductReviewsScreen extends StatelessWidget {
   const ProductReviewsScreen({super.key});
@@ -21,7 +13,7 @@ class ProductReviewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       /// App bar
-      appBar: TAppBar(
+      appBar: const TAppBar(
         title: Text('Reviews & Ratings'),
         showBackArrow: true,
       ),
@@ -29,19 +21,19 @@ class ProductReviewsScreen extends StatelessWidget {
       ///body
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 "Ratings and reviews are verified and "
                 "are from people who use the same type of device that you use.",
               ),
-              SizedBox(height: TSizes.spaceBtwItems),
+              const SizedBox(height: TSizes.spaceBtwItems),
 
               ///overall product ratings
-              TOverallProductrating(),
-              TRatingBarIndicator(
+              const TOverallProductrating(),
+              const TRatingBarIndicator(
                 rating: 3.5,
               ),
               Text("12,611", style: Theme.of(context).textTheme.bodySmall),
