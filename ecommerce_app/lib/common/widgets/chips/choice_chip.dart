6fbed52276cf97fd8a60466b2a94877c2476b82f
Rashminda <rashminda121@gmail.com>
2024-04-07 20:22:11 +0530

    Update choice_chip.dart

--- ecommerce_app/lib/common/widgets/chips/choice_chip.dart
+++ ecommerce_app/lib/common/widgets/chips/choice_chip.dart
@@ -19,24 +19,29 @@ class TChoiceChip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isColor=THelperFunctions.getColor(text) !=null;
-    return ChoiceChip(
-      label: isColor ? const SizedBox() : Text(text),
-      selected: selected,
-      onSelected: onSelected,
-      labelStyle: TextStyle(color: selected ? TColors.white : null),
-      avatar: isColor? TCircularContainer(width: 50, height: 50, backgroundColor: THelperFunctions.getColor(text)!,) : null,
-      labelPadding: isColor
-          ? const EdgeInsets.all(0)
-          : null, // Make icon in the center
-      padding: isColor
-          ? const EdgeInsets.all(0)
-          : null,
-      shape: isColor
-          ? const CircleBorder()
-          : null,
-      backgroundColor: isColor
-          ? THelperFunctions.getColor(text)!
-          : null,
+    final dark=THelperFunctions.isDarkMode(context);
+
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor ? const SizedBox() : Text(text),
+        selected: selected,
+        onSelected: onSelected,
+        labelStyle: TextStyle(color: selected ? TColors.white : dark?TColors.white:TColors.dark),
+        avatar: isColor? TCircularContainer(width: 50, height: 50, backgroundColor: THelperFunctions.getColor(text)!) : null,
+        labelPadding: isColor
+            ? const EdgeInsets.all(0)
+            : null, // Make icon in the center
+        padding: isColor
+            ? const EdgeInsets.all(0)
+            : null,
+        shape: isColor
+            ? const CircleBorder()
+            : null,
+        backgroundColor: isColor
+            ? THelperFunctions.getColor(text)!
+            : null,
+      ),
     );
   }
 }
