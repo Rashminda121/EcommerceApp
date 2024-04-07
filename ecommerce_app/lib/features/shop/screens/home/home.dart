3ee5b7b1ade29c989bff1208cd5def4bfd6097c1
Rashminda <rashminda121@gmail.com>
2024-04-07 23:31:20 +0530

    Update home.dart

--- ecommerce_app/lib/features/shop/screens/home/home.dart
+++ ecommerce_app/lib/features/shop/screens/home/home.dart
@@ -1,10 +1,12 @@
 import 'package:ecommerce_app/common/widgets/layouts/grid_layout.dart';
 import 'package:ecommerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:ecommerce_app/features/shop/screens/all_products/all_products.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_ctegories.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/container/search_container.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
@@ -76,7 +78,7 @@ class HomeScreen extends StatelessWidget {
                   const SizedBox(height: TSizes.spaceBtwSections),
 
                   ///Heading
-                  TSectionHeading(title: 'Popular Products',onPressed: (){}),
+                  TSectionHeading(title: 'Popular Products',onPressed: ()=>Get.to(()=>const AllProducts())),
                   const SizedBox(height: TSizes.spaceBtwSections),
 
                   ///popular products
