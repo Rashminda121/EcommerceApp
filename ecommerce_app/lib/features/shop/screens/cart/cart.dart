47aecda5a5438154676cfff0bc6e3329779a1042
Rashminda <rashminda121@gmail.com>
2024-04-07 20:22:34 +0530

    Create cart.dart

---
+++ ecommerce_app/lib/features/shop/screens/cart/cart.dart
@@ -0,0 +1,50 @@
+import 'package:ecommerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:ecommerce_app/common/widgets/texts/product_title_text.dart';
+import 'package:ecommerce_app/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
+import 'package:ecommerce_app/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:ecommerce_app/features/shop/screens/checkout/checkout.dart';
+import 'package:ecommerce_app/utils/constants/colors.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce_app/common/widgets/images/t_rounded_image.dart';
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../common/widgets/icons/t_circular_icon.dart';
+import '../../../../common/widgets/products/cart/add_remove_button.dart';
+import '../../../../common/widgets/products/cart/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: TAppBar(showBackArrow: true, title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+
+        ///items in cart
+        child: TCartItems(),
+      ),
+
+      ///checkout button
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(onPressed: ()=>Get.to(()=>const CheckoutScreen()),child: const Text('Checkout \$256.0'),),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
