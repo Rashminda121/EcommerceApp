d71994e0e5594cbe74c1ecb82c4b29a72178d4e9
Rashminda <rashminda121@gmail.com>
2024-04-07 20:22:40 +0530

    Create billing_address_section.dart

---
+++ ecommerce_app/lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/texts/section_heading.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+class TBillingAddressSection extends StatelessWidget {
+  const TBillingAddressSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TSectionHeading(
+          title: 'Shipping Address',
+          buttonTitle: 'Change',
+          onPressed: () {},
+        ),
+        Text('customer name', style: Theme.of(context).textTheme.bodyLarge),
+
+        SizedBox(height: TSizes.spaceBtwItems / 2),
+        Row(
+          children: [
+            Icon(Icons.phone, color: Colors.grey, size: 16),
+            SizedBox(width: TSizes.spaceBtwItems),
+            Text('+92-317-8059525',
+                style: Theme.of(context).textTheme.bodyMedium),
+          ],
+        ),
+        SizedBox(height: TSizes.spaceBtwItems / 2),
+        Row(
+          children: [
+            Icon(Icons.location_history, color: Colors.grey, size: 16),
+            SizedBox(width: TSizes.spaceBtwItems),
+            Expanded(
+              child: Text(
+                'South Liana, Maine 87695, USA',
+                style: Theme.of(context).textTheme.bodyMedium,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
