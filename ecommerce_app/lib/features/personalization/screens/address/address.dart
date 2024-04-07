ce68f42c2a39a99872537282d7ff2bd16fb91538
Rashminda <rashminda121@gmail.com>
2024-04-07 20:22:27 +0530

    Create address.dart

---
+++ ecommerce_app/lib/features/personalization/screens/address/address.dart
@@ -0,0 +1,43 @@
+import 'package:ecommerce_app/features/personalization/screens/address/widgets/single_address.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../common/styles/rounded_container.dart';
+import '../../../../common/widgets/appbar/appbar.dart';
+import '../../../../utils/constants/colors.dart';
+import '../../../../utils/constants/sizes.dart';
+import 'add_new_addres.dart';
+
+class UserAdressScreen extends StatelessWidget {
+  const UserAdressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: TColors.primary,
+        onPressed: () => Get.to(() => const AddNewAddressScreen()),
+        child: const Icon(Iconsax.add, color: TColors.white),
+      ),
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text('Addresses', style: Theme.of(context).textTheme.headlineSmall),
+      ),
+
+      body: const SingleChildScrollView(
+        child: Padding (
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column (
+            children: [
+                TSingleAddress(selectedAddress: false),
+              TSingleAddress(selectedAddress: true),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
