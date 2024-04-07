43b7c749884edc02babd74aee50b13cec54a9a5e
Rashminda <rashminda121@gmail.com>
2024-04-07 23:31:13 +0530

    Update cart.dart

--- ecommerce_app/lib/features/shop/screens/cart/cart.dart
+++ ecommerce_app/lib/features/shop/screens/cart/cart.dart
@@ -1,23 +1,11 @@
-import 'package:ecommerce_app/common/widgets/texts/product_price_text.dart';
-import 'package:ecommerce_app/common/widgets/texts/product_title_text.dart';
-import 'package:ecommerce_app/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
 import 'package:ecommerce_app/features/shop/screens/cart/widgets/cart_items.dart';
 import 'package:ecommerce_app/features/shop/screens/checkout/checkout.dart';
-import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/common/widgets/appbar/appbar.dart';
-import 'package:ecommerce_app/common/widgets/images/t_rounded_image.dart';
-import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
-import '../../../../common/widgets/icons/t_circular_icon.dart';
-import '../../../../common/widgets/products/cart/add_remove_button.dart';
-import '../../../../common/widgets/products/cart/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
